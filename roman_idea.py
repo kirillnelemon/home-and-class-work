93a0923659596b4623f955227897e7854b35f498
kirillnelemon <kiragataulin2@gmail.com>
2026-06-03 13:44:11 +0500

    1

---
+++ roman_idea.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import *
+root = Tk()
+root.title("проверка истории")
+root.geometry("300x300")
+
+BG_block = tk.Frame(root, bg="#fff")
+BG_block.grid(row=0, column=0)
+
+lABLLE = tk.Label(BG_block,text="sssssssssssssssssssssss" ,fg="black", bg="#fff")
+lABLLE.grid(row=0, column=1)
+
+
+
+
+
+
+root.mainloop()
